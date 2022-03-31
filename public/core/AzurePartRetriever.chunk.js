f332dd00aeac3867165b1dc7d146b93c48013065
bravo68web <jbandyopadhayaya@gmail.com>
2022-03-31 23:32:20 +0530

    Added WebViewer Support for Resume viewing

---
+++ public/core/AzurePartRetriever.chunk.js
@@ -0,0 +1,3 @@
+/** Notice * This file contains works from many authors under various (but compatible) licenses. Please see core.txt for more information. **/
+(function(){(window.wpCoreControlsBundle=window.wpCoreControlsBundle||[]).push([[5],{389:function(ia,da,e){e.r(da);var ba=e(1),ea=e(138);ia=e(383);e=e(351);ia=function(e){function aa(aa,z,w,y){return e.call(this,aa,z,w,y)||this}Object(ba.c)(aa,e);aa.prototype.OQ=function(){return{start:this.eQ-ea.a,stop:this.eQ}};aa.prototype.Vr=function(aa){var z=this;this.Um(this.url,{start:0,stop:1},function(w,y,r){if(w)return aa(w);w=r.request.getResponseHeader("Content-Range");z.eQ=w.split("/")[1];e.prototype.Vr.call(z,
+aa)})};return aa}(ia["default"]);Object(e.a)(ia);Object(e.b)(ia);da["default"]=ia}}]);}).call(this || window)
---
+++ public/core/WebDBPartRetriever.chunk.js
@@ -0,0 +1,3 @@
+/** Notice * This file contains works from many authors under various (but compatible) licenses. Please see core.txt for more information. **/
+(function(){(window.wpCoreControlsBundle=window.wpCoreControlsBundle||[]).push([[15],{395:function(ia,da,e){e.r(da);var ba=e(1),ea=e(0);e.n(ea);ia=e(88);e=e(351);ia=function(e){function aa(aa,z,w){z=e.call(this,aa,z,w)||this;z.db=aa;return z}Object(ba.c)(aa,e);aa.prototype.request=function(e){var z=this;Object(ea.each)(e,function(e){z.db.get(e,function(w,r,n){return w?z.trigger("partReady",{bb:e,error:w}):z.trigger("partReady",{bb:e,data:r,Uh:!1,Jf:!1,error:null,Vc:n})})})};aa.prototype.Vr=function(e){e()};return aa}(ia.a);
+Object(e.a)(ia);Object(e.b)(ia);da["default"]=ia}}]);}).call(this || window)
