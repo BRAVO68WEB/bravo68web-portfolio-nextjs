72f93968fa14df638f22cc4701f8b314f49d402c
Jyotirmoy Bandyopadhyaya [Bravo68] <jbandyopadhayaya@gmail.com>
2023-01-02 18:16:39 +0530

    Updated Qbadges

--- components/qbadges.js
+++ components/qbadges.js
@@ -1,34 +1,40 @@
-import { ExtraStyles } from "./extras.style";
-import {qb} from "config"
+import { QBStyles } from "./qbadges.style";
+import { qwiklabs as qb } from "config"
+
 function qbadges() {
-  var qbData = Array.from(qb, ([title,earnDate,url,image]) => ({ title,earnDate,url,image }));
-  console.log(qbData);
     return (
-    <ExtraStyles>
+    <QBStyles>
       <div>
         <h1 className="title">Qwiklab Badges</h1>
         <div className="default">
-              {/* <div>
-                <div className="content">
-                  <div className="content-description">
-                    <img src={qb[1].image} />
-                  </div>
-                  <div className="content-title">
-                    <h1>
-                      <a
-                        href={qb[1].url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        {qb[1].title}
-                      </a>
-                    </h1>
+          
+          {qb.map((badge, index) => {
+            return  (
+              <>
+                <div>
+                  <div className="content">
+                    <div className="content-description">
+                      <img src={badge.image} />
+                    </div>
+                    <div className="content-title">
+                      <h1>
+                        <a
+                          href={badge.url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {badge.title}
+                        </a>
+                      </h1>
+                    </div>
                   </div>
                 </div>
-              </div> */}
+            </>
+            )
+          })}
         </div>
       </div>
-    </ExtraStyles>
+    </QBStyles>
   );
 }
 
---
+++ components/qbadges.style.jsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+
+export const QBStyles = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  padding-top: 15vh;
+  background-image: url("images/background6.webp");
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  overflow-y: scroll !important;
+  height: 100vh;
+  .title {
+    font-size: 2rem;
+    font-weight: bold;
+    background-color: var(--color4);
+    color: var(--color3);
+  }
+  .default {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    /* height: 100vh; */
+    padding-bottom: 3em;
+    padding-top: 2em;
+    .content {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      align-items: center;
+      padding-top: 2em;
+      padding-bottom: 2em;
+      &:hover {
+        filter: contrast(200%);
+        -webkit-transform: translateX(-5px) translateY(-5px);
+        transform: translateX(-5px) translateY(-5px);
+        transition: 1s ease;
+      }
+      .content-description {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+      }
+      .content-description img {
+        width: 225px;
+      }
+      .content-title {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-wrap: wrap;
+        word-break: break-word;
+        white-space: pre-wrap;
+        -moz-white-space: pre-wrap;
+        justify-content: center;
+        align-items: center;
+        h1 {
+          font-size: 1.5em;
+          font-weight: bold;
+          color: var(--color7);
+          text-align: center;
+          margin: 0;
+          padding: 0;
+          a {
+            color: var(--color7);
+            text-decoration: none;
+            &:hover {
+              color: var(--color3);
+            }
+          }
+        }
+      }
+    }
+  }
+`;
